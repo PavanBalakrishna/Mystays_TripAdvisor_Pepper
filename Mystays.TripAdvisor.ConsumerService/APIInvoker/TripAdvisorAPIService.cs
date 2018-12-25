d8c5aefc6457b61225e72a9e5add42f7a3948f60
Pavan Balakrishna <pavan.hbalakrishna@gmail.com>
2018-12-25 17:41:59 +0900

    Update

--- Mystays.TripAdvisor.ConsumerService/APIInvoker/TripAdvisorAPIService.cs
+++ Mystays.TripAdvisor.ConsumerService/APIInvoker/TripAdvisorAPIService.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using Mystays.TripAdvisor.ConsumerService.Models.Activity;
+using Amazon.Lambda.Core;
+using System;
 
 namespace Mystays.TripAdvisor.ConsumerService.APIInvoker
 {
@@ -12,11 +11,13 @@ namespace Mystays.TripAdvisor.ConsumerService.APIInvoker
     {
         public static string GetActivities()
         {
+            string TAkey = Environment.GetEnvironmentVariable("TAKey");
             using (var httpClient = new HttpClient())
             {
+                
                 httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                string url = "http://api.tripadvisor.com/api/partner/2.0/location/60745/attractions?key=e84a5b42-e036-403a-b5a3-ca393296b27a";
+                string url = string.Format("http://api.tripadvisor.com/api/partner/2.0/location/60745/attractions?key={0}",TAkey);
                 var response = httpClient.GetAsync(url).Result;
                 ActivityRootObject activityRootObject = JsonConvert.DeserializeObject<ActivityRootObject>(response.Content.ReadAsStringAsync().Result);
                 return activityRootObject.data[0].description;
